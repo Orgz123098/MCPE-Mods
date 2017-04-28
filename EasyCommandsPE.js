3c7a29ad8846d774c5c0693eca5fa7e9bd7bfb6c
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-04-28 11:13:16 -0500

    Update EasyCommandsPE.js

--- EasyCommandsPE.js
+++ EasyCommandsPE.js
@@ -21,4 +21,60 @@ function procCmd(command){
       }
     }
   }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "4"){
+        Player.addItemInventory(1, 4, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "5"){
+        Player.addItemInventory(1, 5, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "10"){
+        Player.addItemInventory(1, 10, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "20"){
+        Player.addItemInventory(1, 3, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "30"){
+        Player.addItemInventory(1, 3, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "40"){
+        Player.addItemInventory(1, 3, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "50"){
+        Player.addItemInventory(1, 3, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "64"){
+        Player.addItemInventory(1, 3, 0);
+      }
+    }
+  }
 }
