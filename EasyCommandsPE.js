f749bc35b68d2350776f6ca86177839abf95ac47
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-04-28 11:18:03 -0500

    Update EasyCommandsPE.js

--- EasyCommandsPE.js
+++ EasyCommandsPE.js
@@ -45,126 +45,168 @@ function procCmd(command){
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "20"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 20, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "30"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 30, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "40"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 40, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "41"){
+        Player.addItemInventory(1, 41, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "42"){
+        Player.addItemInventory(1, 42, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "43"){
+        Player.addItemInventory(1, 43, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "44"){
+        Player.addItemInventory(1, 44, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "45"){
+        Player.addItemInventory(1, 45, 0);
+      }
+    }
+  }
+  if(cmd[0] == "give"){
+    if(cmd[1] == "1"){
+      if(cmd[2] == "46"){
+        Player.addItemInventory(1, 46, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "50"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 50, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "51"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 51, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "52"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 52, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "53"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 53, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "54"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 54, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "55"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 55, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "56"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 56, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "57"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 57, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "58"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 58, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "59"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 59, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "60"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 60, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "61"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 61, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "62"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 62, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "63"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 63, 0);
       }
     }
   }
   if(cmd[0] == "give"){
     if(cmd[1] == "1"){
       if(cmd[2] == "64"){
-        Player.addItemInventory(1, 3, 0);
+        Player.addItemInventory(1, 64, 0);
       }
     }
   }
