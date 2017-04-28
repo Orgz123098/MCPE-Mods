91adc6ab465b339fc633f1c4ef89fe99d1852a7f
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-04-28 11:11:27 -0500

    Update Essentials.js

--- Essentials.js
+++ Essentials.js
@@ -1,29 +1,4 @@
 //Commands
-function procCmd(command){
-  var cmd = command.split(" ");
-  if(cmd[0] == "give"){
-    if(cmd[1] == "1"){
-      if(cmd[2] == "1"){
-        Player.addItemInventory(1, 1, 0);
-      }
-    }
-  }
-  if(cmd[0] == "give"){
-    if(cmd[1] == "1"){
-      if(cmd[2] == "2"){
-        Player.addItemInventory(1, 2, 0);
-      }
-    }
-  }
-  if(cmd[0] == "give"){
-    if(cmd[1] == "1"){
-      if(cmd[2] == "3"){
-        Player.addItemInventory(1, 3, 0);
-      }
-    }
-  }
-}
-
 //Colors
 var black = android.graphics.Color.BLACK;
 var white = android.graphics.Color.WHITE;
